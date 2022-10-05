f4965f6909754f683afd6918c45b488b773e9398
ilyass <ex_p@live.com>
2022-10-05 12:30:00 +0300

    digital numbers

--- main.py
+++ main.py
@@ -1,42 +1,81 @@
-import random
-
-try:
-    import piperclip
-except ImportError:
-    pass
+import sevseg
+myNumber = sevseg.getSevSegStr(14,9)
+print(myNumber)
 
 
-def main():
-    print(''' sPoNgECasE, by asiancart eNTeR yOuR MesSaGe:''')
+def getSevSegStr(number,minWidth=0):
+    """Return a seven-segment display string of number. The returned
+    string will be padded with zeros if it is smaller than minWidth."""
 
-    spongetext = englishToSpongecase(input('> '))
-    print()
-    print(spongetext)
 
-    try:
-        pyperclip.copy(spongetext)
-        print('(cOpIed SpOnGeTexT to ClIpbOaRd.)')
-    except:
-        pass
+    number = str(number).zfill(minWidth)
 
-def englishToSpongecase(message):
-    spongetext = ''
-    useUpper = False
-
-    for character in message:
-        if not character.isalpha():
-            spongetext += character
+    rows = ['','','']
+    for i,numeral in enumerate(number):
+        if numeral == '.':
+            rows[0] +=' '
+            rows[1] +=' '
+            rows[2] +='.'
             continue
+        elif numeral == '-':
+            rows[0] += '    '
+            rows[1] += ' __ '
+            rows[2] += '    '
+        elif numeral == '0':
+            rows[0] += ' __ '
+            rows[1] += '|  |'
+            rows[2] += '|__|'
+        elif numeral == '1':
+            rows[0] += '    '
+            rows[1] += '   |'
+            rows[2] += '   |'
+        elif numeral == '2':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += '|__ '
+        elif numeral == '3':
+            rows[0] += ' __ '
+            rows[1] += ' __|'
+            rows[2] += ' __|'
+        elif numeral == '4':
+            rows[0] += '    '
+            rows[1] += '|__|'
+            rows[2] += '   |'
+        elif numeral == '5':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += ' __|'
+        elif numeral == '6':
+            rows[0] += ' __ '
+            rows[1] += '|__ '
+            rows[2] += '|__|'
+        elif numeral == '7':
+            rows[0] += ' __ '
+            rows[1] += '  | '
+            rows[2] += '  | '
+        elif numeral == '8':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += '|__|'
+        elif numeral == '9':
+            rows[0] += ' __ '
+            rows[1] += '|__|'
+            rows[2] += ' __|'
 
-        if useUpper:
-            spongetext += character.upper()
-        else:
-            spongetext += character.lower()
 
-        if random.randint(1,100) <= 90:
-            useUpper = not useUpper
-    return spongetext
+        if i != len(number) -1:
+            rows[0] += ' '
+            rows[1] += ' '
+            rows[2] += ' '
 
+    return '\n'.join(rows)
 
 if __name__ == '__main__':
-    main()
+    print(' this program need import sevseg')
+    print(' change myNumber value and get digital numbers')
+    print(' first value is number , second value is how many digits you want (for example if you say (42,4) it will print 0042 .its add 0 to begining')
+
+
+
+
+
